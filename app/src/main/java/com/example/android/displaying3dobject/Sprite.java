9441eb7a897f0a6b6dd0ee465a0faa1d61ba0a43
Shrung <shrungbhatt@gmail.com>
2018-06-22 13:40:24 +0530

    Loaded 2d image successfully

--- app/src/main/java/com/example/android/displaying3dobject/Sprite.java
+++ app/src/main/java/com/example/android/displaying3dobject/Sprite.java
@@ -29,19 +29,14 @@ public class Sprite {
     private final int mTextureCoordinateDataSize = 2;
 
 
-
     private final String vertexShaderCode =
-//Test
             "attribute vec2 a_TexCoordinate;" +
                     "varying vec2 v_TexCoordinate;" +
-//End Test
                     "uniform mat4 uMVPMatrix;" +
                     "attribute vec4 vPosition;" +
                     "void main() {" +
-                    "  gl_Position = vPosition * uMVPMatrix;" +
-                    //Test
-                    "v_TexCoordinate = a_TexCoordinate" +
-                    //End Test
+                    "  gl_Position = uMVPMatrix * vPosition;" +
+                    "v_TexCoordinate = a_TexCoordinate;" +
                     "}";
 
     private final String fragmentShaderCode =
@@ -63,28 +58,19 @@ public class Sprite {
 
     };
 
-    final float[] cubeTextureCoordinateData =
-            {
-                    //Front face
-                    /*0.0f, 0.0f,
-                    0.0f, 1.0f,
-                    1.0f, 0.0f,
-                    0.0f, 1.0f,
-                    1.0f, 1.0f,
-                    1.0f, 0.0f*/
-
-                    -0.5f, 0.5f,
-                    -0.5f, -0.5f,
-                    0.5f, -0.5f,
-                    0.5f, 0.5f
-            };
+    final float[] cubeTextureCoordinateData = {
+            0.0f,  0.0f,
+            1.0f, 0.0f,
+            1.0f, 1.0f,
+            0.0f,  1.0f
+    };
 
 
     private short drawOrder[] = {0, 1, 2, 0, 2, 3}; //Order to draw vertices
     private final int vertexStride = COORDS_PER_VERTEX * 4; //Bytes per vertex
 
     // Set color with red, green, blue and alpha (opacity) values
-    float color[] = {0.63671875f, 0.76953125f, 0.22265625f, 1.0f};
+    float color[] = {1.0f, 1.0f, 1.0f, 1.0f};
 
     public Sprite(Context context) {
 
@@ -106,7 +92,7 @@ public class Sprite {
 
         GLES20.glLinkProgram(mProgram);
 
-        mTextureHandle = loadTexture(mContext, R.mipmap.ic_launcher);
+        mTextureHandle = loadTexture(mContext, R.drawable.android);
 
         mCubeTextureCoordinates = ByteBuffer.allocateDirect(cubeTextureCoordinateData.length * 4).
                 order(ByteOrder.nativeOrder()).asFloatBuffer();
@@ -141,14 +127,18 @@ public class Sprite {
         //Enable a handle to the triangle vertices
         GLES20.glEnableVertexAttribArray(mPositionHandle);
 
+
+
         //Prepare the triangle coordinate data
-        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, mVertexBuffer);
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT,
+                false, vertexStride, mVertexBuffer);
 
         //Get Handle to Fragment Shader's vColor member
         mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
 
         //Set the Color for drawing the triangle
         GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+
 
         //Set Texture Handles and bind Texture
         mTextureUniformHandle = GLES20.glGetAttribLocation(mProgram, "u_Texture");
@@ -166,7 +156,7 @@ public class Sprite {
         //Pass in the texture coordinate information
         mCubeTextureCoordinates.position(0);
         GLES20.glVertexAttribPointer(mTextureCoordinateHandle, mTextureCoordinateDataSize, GLES20.GL_FLOAT,
-                false, 0, mCubeTextureCoordinates);
+                false, 8, mCubeTextureCoordinates);
         GLES20.glEnableVertexAttribArray(mTextureCoordinateHandle);
 
         //Get Handle to Shape's Transformation Matrix
@@ -198,8 +188,8 @@ public class Sprite {
             GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle[0]);
 
             // Set filtering
-            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
-            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
 
             // Load the bitmap into the bound texture.
             GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
